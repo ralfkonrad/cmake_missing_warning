6ccabb21686697afe5cad7c140e79c1b59ffd7d0
RalfKonrad <github@eckel-consulting.de>
2026-02-04 11:26:58 +0100

    Add workflows

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include <boost/multi_array.hpp>
 
-int main() {
-    std::cout << "Hello World!" << std::endl;
+int main()
+{
+    boost::multi_array<double, 3> d(boost::extents[2][2][2]);
+    d[0][0][0] = 1.0;
     return 0;
 }
